9f2d827619fc1e8105c137e9d22f82749c6c34d7
anushkrishnav <anush.venkatakrishna@gmail.com>
2021-08-01 05:08:33 +0530

    members page

--- src/ReduxTable/peopleSlice.js
+++ src/ReduxTable/peopleSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import{ useState } from 'react';
+import axios from 'axios';
 let nextIdVal = 0;
 
 export function nextID() {
@@ -7,17 +8,46 @@ export function nextID() {
   return nextIdVal;
 }
 
+
+function getMemberData() {
+  const [memdata, setMemdata] = useState([]);
+  const response = axios.get('https://mile12db.azurewebsites.net/api/coop/users/C-1').then(res => {
+  let data = res.data.map(
+      obj => {
+        return {
+          "name" : obj.name,
+          "phoneNumber" : obj.Mobile,
+          "tag" : obj.Stauts,
+          "id" : nextID()
+        }
+      }
+    );
+    setMemdata(data)
+    return data;
+
+
+  })
+  
+};
+let memdata = getMemberData();
+
+// let memdata = [
+//    this is just an initial example... we need to display the information of people that were added from the back end
+//   { name: "Ireti", phoneNumber: "251-456-342", tag: "leader", id: nextID() },
+//   { name: "Yemi", phoneNumber: "251-456-789",  tag: "packager", id: nextID() },
+// ]
+
+console.log(getMemberData());
 export const peopleSlice = createSlice({
   name: "people",
   initialState: {
-    list: [
-      // this is just an initial example... we need to display the information of people that were added from the back end
-      { name: "Ireti", phoneNumber: "251-456-342", tag: "leader", id: nextID() },
-      { name: "Yemi", phoneNumber: "251-456-789",  tag: "packager", id: nextID() },
-    ],
-    loading: false,
+    list: [],
+    loading: true,
   },
   reducers: {
+    getuserData: (state, action) => {
+      
+    },
     add: (state, action) => {
       state.list.push(action.payload);
     },
